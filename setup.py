1420da443d409e51c8070b13d529856fa0f75092
Kevdagoat <kevdagoat@gmail.com>
2020-01-02 20:42:05 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+     name='crafty-web-client',
+     version='0.99A',
+     description='A python library for talking to Crafty Web minecraft server control panel',
+     author='kevdagoat',
+     url='https://gitlab.com/crafty-controller/crafty-web-client',
+     packages=['crafty-web-client']
+)
