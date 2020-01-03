44e6c1fc07b16bb38ddf0c75cbb0da4f605250b9
Kevdagoat <kevdagoat@gmail.com>
2020-01-03 17:30:26 +0800

    fix all issues with pip

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
 setup(
-     name='crafty-web-client',
+     name='crafty-client',
      version='0.99A',
      description='A python library for talking to Crafty Web minecraft server control panel',
      author='kevdagoat',
-     url='https://gitlab.com/crafty-controller/crafty-web-client',
+     url='https://gitlab.com/crafty-controller/crafty-client',
+     install_requires=[
+          'requests',
+          'urllib3',
+      ],
      packages=['crafty-web-client']
 )
