fbb594e81b0e581f8175118e2384d0233dda5261
Ptarrant <ptarrant@gmail.com>
2020-01-03 19:41:56 -0500

    edits to the setup.py to include long description md from readme

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
      name='crafty_client',
      version='1.0.0',
      description='A python library for talking to Crafty Web minecraft server control panel',
+     long_description=long_description,
+     long_description_content_type="text/markdown",
      author='kevdagoat',
      url='https://gitlab.com/crafty-controller/crafty-client',
      install_requires=[
